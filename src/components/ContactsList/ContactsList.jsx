4a4513551ba0db89de9c0a52e423efb0c6fe4970
Tetiana-UA <izidaxm85@gmail.com>
2024-01-23 17:18:39 +0100

    8

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -2,7 +2,11 @@ import styles from "./contacts-list.module.css";
 
 const ContactsList = ({items,deleteContact}) => {
     const elements=items.map(({id, name, number}) =>
-    <li className={styles.listLi} key={id}>{name}:{number} <button className={styles.listButton} onClick={()=>deleteContact(id)} type="button">Delete</button></li>)
+    <li 
+    className={styles.listLi} 
+    key={id}>{name}:  {number} 
+    <button className={styles.listButton} onClick={()=>deleteContact(id)} type="button">Delete</button>
+    </li>)
 
     return (
         <ol className={styles.list}>
